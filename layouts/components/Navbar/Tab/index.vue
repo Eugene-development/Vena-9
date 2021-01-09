01f4e1cc5a16f533d0274e8c39924874d0543a6e
Eugene Che <indexpro24@gmail.com>
2021-01-09 14:36:11 +0300

    tab8

--- layouts/components/Navbar/Tab/index.vue
+++ layouts/components/Navbar/Tab/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div  class="px-6 py-1 my-3">
-    <span v-for="(tab, i) of tabs"  class="shadow inline-flex items-center px-3 py-2 mr-2 rounded text-xs font-medium leading-4 bg-violet-100 text-gray-800">
+    <span v-for="(tab, i) of tabs" class="shadow inline-flex items-center px-3 py-2 mr-2 rounded text-xs font-medium leading-4 bg-gray-100 text-gray-800">
       <NuxtLink :to="tab.path">
         {{ tab.name }}
       </NuxtLink>
-      <button
-        @click="closeTab (tab.path)"
-      >
-        <svg class="ml-3  h-3 w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <button @click="closeTab (tab.path)">
+        <svg class="ml-3 h-3 w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path clip-rule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                 fill-rule="evenodd"/>
